0dee9de1993c1f03401fad68e24c8b797d110877
Mohd Rejoan <wearecrx@gmail.com>
2023-01-14 01:29:18 +0600

    add tailwind custom configration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
     content: ["./**/*.{html}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                FF008A: "#FF008A",
+                "98A2B3": "#98A2B3",
+                EAEDF3: "#EAEDF3",
+            },
+            borderRadius: {
+                "6px": "6px",
+            },
+            fontFamily: {
+                "inter" : "'Inter', sans-serif"
+            }
+        },
     },
     plugins: [],
 };
