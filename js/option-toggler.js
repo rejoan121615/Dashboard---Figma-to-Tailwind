d140f8a14b1a171c3da3f20ced9ada9e148c9478
Mohd Rejoan <wearecrx@gmail.com>
2023-01-14 22:26:38 +0600

    js conflict fix

--- js/option-toggler.js
+++ js/option-toggler.js
@@ -1,14 +1,12 @@
 const x = document.querySelector(".option_btn");
-let toggler = true
+let toggler = true;
 
 x.onclick = () => {
-  if (a == true) {
-    toggler = false
-    document.querySelector(".demo").style.display = "block";
-  }
-
-  else{
-    toggler = true
-    document.querySelector(".demo").style.display = "none";
-  }
+    if (toggler == true) {
+        toggler = false;
+        document.querySelector(".options_panel").style.display = "block";
+    } else {
+        toggler = true;
+        document.querySelector(".options_panel").style.display = "none";
+    }
 };
