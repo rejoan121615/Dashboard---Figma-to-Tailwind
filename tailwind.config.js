b3560e5b6f3720900071937d64880f365dbd055f
Mohd Rejoan <wearecrx@gmail.com>
2023-01-14 11:18:43 +0600

    recording card complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
                 FF008A: "#FF008A",
                 "98A2B3": "#98A2B3",
                 EAEDF3: "#EAEDF3",
+                F9FAFD: "#F9FAFD",
             },
             borderRadius: {
                 "6px": "6px",
@@ -19,11 +20,11 @@ module.exports = {
             },
             lineHeight: {
                 "19px": "19px",
-                '48px': "48px"
+                "48px": "48px",
             },
             fontWeight: {
-                "500": 500
-            }
+                500: 500,
+            },
         },
     },
     plugins: [],
